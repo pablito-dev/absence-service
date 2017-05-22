4327e6da745d58e2b8802cacd6532b4d1825b955
Paweł Nowak <pawenow684@student.polsl.pl>
2017-05-22 23:01:46 +0200

    Add standard spring boot app

---
+++ src/main/java/com/myhr/myhr/AbsenceServiceApplication.java
@@ -0,0 +1,12 @@
+package com.myhr.myhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenceServiceApplication.class, args);
+	}
+}
