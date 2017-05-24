35f834a39b6584df93736d828019b34830974a55
Paweł Nowak <pavelvaremreis@gmail.com>
2017-05-24 21:26:01 +0200

    Add request date, start and end date to absence model

--- src/main/java/com/myhr/myhr/model/Absence.java
+++ src/main/java/com/myhr/myhr/model/Absence.java
@@ -2,6 +2,8 @@ package com.myhr.myhr.model;
 
 import org.springframework.data.annotation.Id;
 
+import java.util.Date;
+
 /**
  * Created by pavel_000 on 23/05/2017.
  */
@@ -11,12 +13,18 @@ public class Absence {
     private String userId;
     private String organizationId;
     private STATUS status;
+    private Date absenceStart;
+    private Date absenceEnd;
+    private Date requestedAt;
 
     Absence(final Builder builder) {
         this.id = builder.id;
         this.userId = builder.userId;
         this.organizationId = builder.organizationId;
         this.status = builder.status;
+        this.absenceStart = builder.absenceStart;
+        this.absenceEnd = builder.absenceEnd;
+        this.requestedAt = builder.requestedAt;
     }
 
     public Absence() {
@@ -38,11 +46,26 @@ public class Absence {
         return status;
     }
 
+    public Date getAbsenceStart() {
+        return absenceStart;
+    }
+
+    public Date getAbsenceEnd() {
+        return absenceEnd;
+    }
+
+    public Date getRequestedAt() {
+        return requestedAt;
+    }
+
     public static class Builder {
         private String id;
         private String userId;
         private String organizationId;
         private STATUS status;
+        private Date absenceStart;
+        private Date absenceEnd;
+        private Date requestedAt;
 
         public Builder setId(final String id) {
             this.id = id;
@@ -61,6 +84,21 @@ public class Absence {
 
         public Builder setStatus(STATUS status) {
             this.status = status;
+            return this;
+        }
+
+        public Builder setAbsenceStart(Date absenceStart) {
+            this.absenceStart = absenceStart;
+            return this;
+        }
+
+        public Builder setAbsenceEnd(Date absenceEnd) {
+            this.absenceEnd = absenceEnd;
+            return this;
+        }
+
+        public Builder setRequestedAt(Date requestedAt) {
+            this.requestedAt = requestedAt;
             return this;
         }
 
