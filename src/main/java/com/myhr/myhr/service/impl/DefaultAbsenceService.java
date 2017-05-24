dd9c767c7873de86116138607e146c5d05e37a2f
Paweł Nowak <pavelvaremreis@gmail.com>
2017-05-24 21:26:57 +0200

    Add logic to query absences using request, start and end date

--- src/main/java/com/myhr/myhr/service/impl/DefaultAbsenceService.java
+++ src/main/java/com/myhr/myhr/service/impl/DefaultAbsenceService.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 /**
@@ -31,21 +31,43 @@ public class DefaultAbsenceService implements AbsenceService {
         final String organizationId = request.pathVariable("organizationId");
 
         return absenceRepository.findByUserIdAndOrganizationId(userId, organizationId)
-                .filter(matchStatus(request));
+                .filter(matchStatus(request))
+                .filter(matchStartDate(request, "absenceStartDate"))
+                .filter(matchEndDate(request, "absenceEndDate"))
+                .filter(matchStartDate(request, "requestStartDate"))
+                .filter(matchEndDate(request, "requestEndDate"));
     }
 
     @Override
-    public Flux<Absence> getAllForOrganization(ServerRequest request) {
+    public Flux<Absence> getAllForOrganization(final ServerRequest request) {
         final String organizationId = request.pathVariable("organizationId");
 
         return absenceRepository.findByOrganizationId(organizationId)
-                .filter(matchStatus(request));
+                .filter(matchStatus(request))
+                .filter(matchStartDate(request, "absenceStartDate"))
+                .filter(matchEndDate(request, "absenceEndDate"))
+                .filter(matchStartDate(request, "requestStartDate"))
+                .filter(matchEndDate(request, "requestEndDate"));
     }
 
-    private Predicate<Absence> matchStatus(ServerRequest request) {
+    private Predicate<Absence> matchStatus(final ServerRequest request) {
         final List<String> requestStatuses = request.queryParams("status");
 
         return i -> requestStatuses.isEmpty() ||
                 requestStatuses.stream().anyMatch(t -> t.equals(i.getStatus().toString()));
+    }
+
+    private Predicate<Absence> matchStartDate(final ServerRequest request, final String paramName) {
+        final Optional<String> startDate = request.queryParam(paramName);
+
+        return i -> !startDate.isPresent() ||
+                i.getAbsenceStart().getTime() >= Long.valueOf(startDate.get());
+    }
+
+    private Predicate<Absence> matchEndDate(final ServerRequest request, final String paramName) {
+        final Optional<String> endDate = request.queryParam(paramName);
+
+        return i -> !endDate.isPresent() ||
+                i.getAbsenceEnd().getTime() <= Long.valueOf(endDate.get());
     }
 }
